77e7f0834897d80608b179b30909edd4fa7d27ad
Kattenkvvaad <115976372+Kattenkvvaad@users.noreply.github.com>
2025-03-16 07:08:26 +0100

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1E1IOJ2lHWR9zgS0YjPRZ3mHuVh-hXPj7vVRRKTyUNeQ";
 
 
 /* ------------------------------------------------------------------- */
