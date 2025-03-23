29f0b947ecb1f1c17646171165ab94390ccd7321
Kattenkvvaad <115976372+Kattenkvvaad@users.noreply.github.com>
2025-03-23 12:46:15 +0100

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -140,9 +140,7 @@ let options = {
         sheetPage: sheetPages.faq,
     
         itemAmount: 24,
-        itemOrder: "asc",
-    
-        searchFilterParams: ['Tags'],
+        itemOrder: "desc",
     
     },
 
